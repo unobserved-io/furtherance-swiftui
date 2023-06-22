ec65ccb24f1e5f6043ff28c9b6d210edd0af9f86
RIcky Kresslein <ricky@lakoliu.com>
2023-06-22 11:37:00 +0200

    Import CSVs

--- Furtherance/FurtheranceApp.swift
+++ Furtherance/FurtheranceApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
@@ -26,6 +27,8 @@ struct FurtheranceApp: App {
     @State private var dialogMessage = ""
     @State private var confirmBtn = ""
     @State private var addTaskSheet = false
+    @State private var showImportCSV = false
+    @State private var showInvalidCSVAlert = false
     @State(initialValue: 0) var tasksCount: Int
     @State(initialValue: []) var navPath: [String]
     @State(initialValue: false) var showExportCSV: Bool
@@ -100,6 +103,49 @@ struct FurtheranceApp: App {
                 } message: {
                     Text("That feature is only available in Furtherance Pro.")
                 }
+                .fileImporter(isPresented: $showImportCSV, allowedContentTypes: [UTType.commaSeparatedText]) { result in
+                    do {
+                        let fileURL = try result.get()
+                        if fileURL.startAccessingSecurityScopedResource() {
+                            let data = try String(contentsOf: fileURL)
+                            // Split string into rows
+                            var rows = data.components(separatedBy: "\n")
+                            // Remove headers
+                            if rows[0] == "Name,Tags,Start Time,Stop Time,Total Seconds" {
+                                rows.removeFirst()
+                                
+                                let localDateFormatter = DateFormatter()
+                                localDateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                                // Split rows into columns
+                                var furTasks = [FurTask]()
+                                for row in rows {
+                                    let columns = row.components(separatedBy: ",")
+                                    
+                                    if columns.count == 5 {
+                                        let task = FurTask(context: persistenceController.container.viewContext)
+                                        task.id = UUID()
+                                        task.name = columns[0]
+                                        task.tags = columns[1]
+                                        task.startTime = localDateFormatter.date(from: columns[2])
+                                        task.stopTime = localDateFormatter.date(from: columns[3])
+                                        furTasks.append(task)
+                                    }
+                                }
+                                try? persistenceController.container.viewContext.save()
+                            } else {
+                                showInvalidCSVAlert.toggle()
+                            }
+                        }
+                        fileURL.stopAccessingSecurityScopedResource()
+                    } catch {
+                        print("Failed to import data: \(error.localizedDescription)")
+                    }
+                }
+                .alert("Invalid CSV", isPresented: $showInvalidCSVAlert) {
+                    Button("OK") { }
+                } message: {
+                    Text("The CSV you chose is not a valid Furtherance CSV.")
+                }
         }
         .commands {
             CommandMenu("Database") {
@@ -108,6 +154,14 @@ struct FurtheranceApp: App {
                         showProAlert.toggle()
                     } else {
                         showExportCSV.toggle()
+                    }
+                }
+                .disabled(tasksCount == 0)
+                Button("Import CSV") {
+                    if storeModel.purchasedIds.isEmpty {
+                        showProAlert.toggle()
+                    } else {
+                        showImportCSV.toggle()
                     }
                 }
                 .disabled(tasksCount == 0)
