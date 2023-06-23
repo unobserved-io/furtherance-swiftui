fa916c8c916c5751414a49ce59ac5c519a2a6069
RIcky Kresslein <ricky@lakoliu.com>
2023-06-23 14:32:26 +0200

    Simplify class

--- Furtherance/Helpers/CSVFile.swift
+++ Furtherance/Helpers/CSVFile.swift
@@ -9,26 +9,22 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 struct CSVFile: FileDocument {
-    // tell the system we support only plain text
     static var readableContentTypes = [UTType.commaSeparatedText]
     static var writableContentTypes = [UTType.commaSeparatedText]
-
-    // by default our document is empty
     var text = ""
 
-    // a simple initializer that creates new, empty documents
     init(initialText: String = "") {
         text = initialText
     }
 
-    // this initializer loads data that has been saved previously
+    // This initializer loads data that has been saved previously
     init(configuration: ReadConfiguration) throws {
         if let data = configuration.file.regularFileContents {
             text = String(decoding: data, as: UTF8.self)
         }
     }
 
-    // this will be called when the system wants to write our data to disk
+    // This will be called when the system wants to write our data to disk
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
         let data = Data(text.utf8)
         return FileWrapper(regularFileWithContents: data)
