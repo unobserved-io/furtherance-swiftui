134c5780412cc5556a5ee874f17f03b68bc39330
RIcky Kresslein <ricky@lakoliu.com>
2023-07-02 10:13:17 +0200

    Add Idle section header

--- Furtherance/Views/Settings/AdvancedSettingsView.swift
+++ Furtherance/Views/Settings/AdvancedSettingsView.swift
@@ -18,34 +18,34 @@ struct AdvancedSettingsView: View {
         Form {
             BuyProView()
 
-//            Section(header: storeModel.purchasedIds.isEmpty ? Text("Idle (Pro)").bold() : Text("Idle").bold()) {
-            HStack {
-                Text("Detect when user is idle")
-                Spacer()
-                Toggle("Detect when user is idle", isOn: $idleDetect)
-                    .toggleStyle(.switch)
-                    .labelsHidden()
-                    .disabled(storeModel.purchasedIds.isEmpty)
-            }
-            .frame(maxWidth: .infinity, maxHeight: 15, alignment: .leading)
-            .padding()
-            .background(colorScheme == .light ? .white.opacity(0.50) : .white.opacity(0.10))
-            .cornerRadius(20)
+            Section(header: storeModel.purchasedIds.isEmpty ? Text("Idle (Pro)").bold() : Text("Idle").bold()) {
+                HStack {
+                    Text("Detect when user is idle")
+                    Spacer()
+                    Toggle("Detect when user is idle", isOn: $idleDetect)
+                        .toggleStyle(.switch)
+                        .labelsHidden()
+                        .disabled(storeModel.purchasedIds.isEmpty)
+                }
+                .frame(maxWidth: .infinity, maxHeight: 15, alignment: .leading)
+                .padding()
+                .background(colorScheme == .light ? .white.opacity(0.50) : .white.opacity(0.10))
+                .cornerRadius(20)
 
-            HStack {
-                Text("Minutes before user is idle:")
-                Spacer()
-                Text("\(idleLimit)")
-                    .bold()
-                Stepper("\(idleLimit)", value: $idleLimit, in: 1 ... 999)
-                    .labelsHidden()
-                    .disabled(storeModel.purchasedIds.isEmpty)
+                HStack {
+                    Text("Minutes before user is idle:")
+                    Spacer()
+                    Text("\(idleLimit)")
+                        .bold()
+                    Stepper("\(idleLimit)", value: $idleLimit, in: 1 ... 999)
+                        .labelsHidden()
+                        .disabled(storeModel.purchasedIds.isEmpty)
+                }
+                .frame(maxWidth: .infinity, maxHeight: 15, alignment: .leading)
+                .padding()
+                .background(colorScheme == .light ? .white.opacity(0.50) : .white.opacity(0.10))
+                .cornerRadius(20)
             }
-            .frame(maxWidth: .infinity, maxHeight: 15, alignment: .leading)
-            .padding()
-            .background(colorScheme == .light ? .white.opacity(0.50) : .white.opacity(0.10))
-            .cornerRadius(20)
-//            }
         }
         .padding(20)
         .frame(width: 400, height: storeModel.purchasedIds.isEmpty ? 200 : 150)
