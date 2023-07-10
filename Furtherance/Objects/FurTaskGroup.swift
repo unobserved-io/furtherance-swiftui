ef3b8a84be0e7a5947098ae78cf877d800f69365
RIcky Kresslein <ricky@lakoliu.com>
2023-07-10 08:37:49 +0200

    Reduce risk of crash by using nil placeholders

--- Furtherance/Objects/FurTaskGroup.swift
+++ Furtherance/Objects/FurTaskGroup.swift
@@ -16,15 +16,15 @@ class FurTaskGroup: Identifiable, ObservableObject {
     var totalTime: Int
 
     init(task: FurTask) {
-        name = task.name!
-        tags = task.tags!
+        name = task.name ?? "Unknown"
+        tags = task.tags ?? ""
         date = localDateFormatter.string(from: task.startTime ?? Date.now)
         tasks.append(task)
-        totalTime = Calendar.current.dateComponents([.second], from: task.startTime!, to: task.stopTime!).second ?? 0
+        totalTime = Calendar.current.dateComponents([.second], from: task.startTime ?? Date.now, to: task.stopTime ?? Date.now).second ?? 0
     }
 
     func add(task: FurTask) {
-        totalTime = totalTime + (Calendar.current.dateComponents([.second], from: task.startTime!, to: task.stopTime!).second ?? 0)
+        totalTime = totalTime + (Calendar.current.dateComponents([.second], from: task.startTime ?? Date.now, to: task.stopTime ?? Date.now).second ?? 0)
         tasks.append(task)
     }
 
