c21ed239c7bc5d183d6e8bff77cd6b41c7d19da6
Ricky Kresslein <ricky@unobserved.io>
2024-07-24 16:30:30 +0200

    Formatting

--- Furtherance/Views/Timer/TaskInputView.swift
+++ Furtherance/Views/Timer/TaskInputView.swift
@@ -18,35 +18,35 @@ struct TaskInputView: View {
     var body: some View {
         TextField("Task Name @Project #tag #another tag $hourly rate", text: $taskTagsInput.text)
             .disabled(stopWatchHelper.pomodoroOnBreak)
-        #if os(iOS)
-        .disableAutocorrection(true)
-        .frame(height: 40)
-        .padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12))
-        .overlay(
-            RoundedRectangle(cornerRadius: 12)
-                .stroke(Color.accentColor, lineWidth: 3)
-        )
-        #else
-        .textFieldStyle(RoundedBorderTextFieldStyle())
-        #endif
-        .onChange(of: taskTagsInput.debouncedText) { _, newVal in
-            if StopWatchHelper.shared.isRunning {
-                if newVal != timerHelper.nameAndTags {
-                    if !newVal.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty,
-                       newVal.trimmingCharacters(in: .whitespaces).first != "#",
-                       newVal.trimmingCharacters(in: .whitespaces).first != "@",
-                       newVal.trimmingCharacters(in: .whitespaces).first != Character(chosenCurrency),
-                       TaskTagsInput.shared.text.count(where: { $0 == "@" }) < 2,
-                       TaskTagsInput.shared.text.count(where: { $0 == Character(chosenCurrency) }) < 2
-                    {
-                        timerHelper.updateTaskAndTagsIfChanged()
-                        #if os(iOS)
-                        timerHelper.updatePersistentTimerTaskName()
-                        #endif
+#if os(iOS)
+            .disableAutocorrection(true)
+            .frame(height: 40)
+            .padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12))
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.accentColor, lineWidth: 3)
+            )
+#else
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+#endif
+            .onChange(of: taskTagsInput.debouncedText) { _, newVal in
+                if StopWatchHelper.shared.isRunning {
+                    if newVal != timerHelper.nameAndTags {
+                        if !newVal.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty,
+                           newVal.trimmingCharacters(in: .whitespaces).first != "#",
+                           newVal.trimmingCharacters(in: .whitespaces).first != "@",
+                           newVal.trimmingCharacters(in: .whitespaces).first != Character(chosenCurrency),
+                           TaskTagsInput.shared.text.count(where: { $0 == "@" }) < 2,
+                           TaskTagsInput.shared.text.count(where: { $0 == Character(chosenCurrency) }) < 2
+                        {
+                            timerHelper.updateTaskAndTagsIfChanged()
+#if os(iOS)
+                            timerHelper.updatePersistentTimerTaskName()
+#endif
+                        }
                     }
                 }
             }
-        }
     }
 }
 
