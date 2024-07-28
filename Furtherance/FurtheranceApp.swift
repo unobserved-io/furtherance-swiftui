c4498f865545f69fa097ff09dbe973a2d289b5ce
Ricky Kresslein <ricky@unobserved.io>
2024-07-28 13:42:49 +0200

    Inspector closes automatically if editing task when all data is deleted

--- Furtherance/FurtheranceApp.swift
+++ Furtherance/FurtheranceApp.swift
@@ -12,100 +12,106 @@ import UniformTypeIdentifiers
 
 @main
 struct FurtheranceApp: App {
-    let persistenceController = PersistenceController.shared
+	let persistenceController = PersistenceController.shared
 
-    #if os(macOS)
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    #endif
-    @AppStorage("launchCount") private var launchCount = 0
-    @AppStorage("showDeleteConfirmation") private var showDeleteConfirmation = true
+	#if os(macOS)
+	@NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	#endif
+	@AppStorage("launchCount") private var launchCount = 0
+	@AppStorage("showDeleteConfirmation") private var showDeleteConfirmation = true
 
-    @ObservedObject var storeModel = StoreModel.shared
+	@ObservedObject var storeModel = StoreModel.shared
 
-    @State private var navigator = Navigator.shared
-    @State private var showDeleteDialog = false
-    @State private var showProAlert = false
-    @State private var dialogTitle = ""
-    @State private var dialogMessage = ""
-    @State private var confirmBtn = ""
-    @State private var addTaskSheet = false
-    @State private var showImportCSV = false
-    @State private var showInvalidCSVAlert = false
-    @State(initialValue: 0) var tasksCount: Int
-    @State(initialValue: false) var showExportCSV: Bool
+	@State private var navigator = Navigator.shared
+	@State private var showDeleteDialog = false
+	@State private var showProAlert = false
+	@State private var dialogTitle = ""
+	@State private var dialogMessage = ""
+	@State private var confirmBtn = ""
+	@State private var addTaskSheet = false
+	@State private var showImportCSV = false
+	@State private var showInvalidCSVAlert = false
+	@State(initialValue: 0) var tasksCount: Int
+	@State(initialValue: false) var showExportCSV: Bool
+	@State(initialValue: false) var showInspector: Bool
+	@State(initialValue: .editTask) var inspectorView: SelectedInspectorView
 
-    init() {
-        launchCount += 1
-    }
+	init() {
+		launchCount += 1
+	}
 
-    var body: some Scene {
-        WindowGroup {
-            mainContentView
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-                .onAppear {
-                    #if os(macOS)
-                    NSWindow.allowsAutomaticWindowTabbing = false
-                    #endif
-                    Task {
-                        try await storeModel.fetchProducts()
-                    }
-                }
-                .confirmationDialog(dialogTitle, isPresented: $showDeleteDialog) {
-                    Button(confirmBtn, role: .destructive) {
-                        if confirmBtn == "Delete" {
-                            deleteAllTasks()
-                        }
-                    }
-                    Button("Cancel", role: .cancel) {}
-                } message: {
-                    Text(dialogMessage)
-                }
-                .sheet(isPresented: $addTaskSheet) {
-                    AddTaskView().environment(\.managedObjectContext, persistenceController.container.viewContext)
-                }
-                .alert("Upgrade to Pro", isPresented: $showProAlert) {
-                    Button("Cancel") {}
-                    if let product = storeModel.products.first {
-                        Button(action: {
-                            Task {
-                                if storeModel.purchasedIds.isEmpty {
-                                    try await storeModel.purchase()
-                                }
-                            }
-                        }) {
-                            Text("Buy Pro \(product.displayPrice)")
-                        }
-                        .keyboardShortcut(.defaultAction)
-                    }
-                } message: {
-                    Text("That feature is only available in Furtherance Pro.")
-                }
-                .fileImporter(isPresented: $showImportCSV, allowedContentTypes: [UTType.commaSeparatedText]) { result in
-                    do {
-                        let fileURL = try result.get()
-                        if fileURL.startAccessingSecurityScopedResource() {
-                            let data = try String(contentsOf: fileURL)
-                            // Split string into rows
-                            var rows = data.components(separatedBy: "\n")
-                            // Remove headers
-                            if rows[0] == "Name,Project,Tags,Rate,Start Time,Stop Time,Total Seconds" || rows[0] == "Name,Tags,Start Time,Stop Time,Total Seconds" {
-                                rows.removeFirst()
+	var body: some Scene {
+		WindowGroup {
+			mainContentView
+				.environment(\.managedObjectContext, persistenceController.container.viewContext)
+				.onAppear {
+					#if os(macOS)
+					NSWindow.allowsAutomaticWindowTabbing = false
+					#endif
+					Task {
+						try await storeModel.fetchProducts()
+					}
+				}
+				.confirmationDialog(dialogTitle, isPresented: $showDeleteDialog) {
+					Button(confirmBtn, role: .destructive) {
+						if confirmBtn == "Delete" {
+							deleteAllTasks()
+							if inspectorView == .editTask || inspectorView == .editTaskGroup {
+								showInspector = false
+								inspectorView = .empty
+							}
+						}
+					}
+					Button("Cancel", role: .cancel) {}
+				} message: {
+					Text(dialogMessage)
+				}
+				.sheet(isPresented: $addTaskSheet) {
+					AddTaskView().environment(\.managedObjectContext, persistenceController.container.viewContext)
+				}
+				.alert("Upgrade to Pro", isPresented: $showProAlert) {
+					Button("Cancel") {}
+					if let product = storeModel.products.first {
+						Button(action: {
+							Task {
+								if storeModel.purchasedIds.isEmpty {
+									try await storeModel.purchase()
+								}
+							}
+						}) {
+							Text("Buy Pro \(product.displayPrice)")
+						}
+						.keyboardShortcut(.defaultAction)
+					}
+				} message: {
+					Text("That feature is only available in Furtherance Pro.")
+				}
+				.fileImporter(isPresented: $showImportCSV, allowedContentTypes: [UTType.commaSeparatedText]) { result in
+					do {
+						let fileURL = try result.get()
+						if fileURL.startAccessingSecurityScopedResource() {
+							let data = try String(contentsOf: fileURL)
+							// Split string into rows
+							var rows = data.components(separatedBy: "\n")
+							// Remove headers
+							if rows[0] == "Name,Project,Tags,Rate,Start Time,Stop Time,Total Seconds" || rows[0] == "Name,Tags,Start Time,Stop Time,Total Seconds" {
+								rows.removeFirst()
 
-                                // Split rows into columns
-                                var furTasks = [FurTask]()
-                                for row in rows {
-                                    let columns = row.components(separatedBy: ",")
+								// Split rows into columns
+								var furTasks = [FurTask]()
+								for row in rows {
+									let columns = row.components(separatedBy: ",")
 
-                                    if columns.count == 7 {
-                                        let task = FurTask(context: persistenceController.container.viewContext)
-                                        task.id = UUID()
-                                        task.name = columns[0]
+									if columns.count == 7 {
+										let task = FurTask(context: persistenceController.container.viewContext)
+										task.id = UUID()
+										task.name = columns[0]
 										task.project = columns[1]
-                                        task.tags = columns[2]
+										task.tags = columns[2]
 										task.rate = Double(columns[3]) ?? 0.0
-                                        task.startTime = localDateTimeFormatter.date(from: columns[4])
-                                        task.stopTime = localDateTimeFormatter.date(from: columns[5])
-                                        furTasks.append(task)
+										task.startTime = localDateTimeFormatter.date(from: columns[4])
+										task.stopTime = localDateTimeFormatter.date(from: columns[5])
+										furTasks.append(task)
 									} else if columns.count == 5 {
 										let task = FurTask(context: persistenceController.container.viewContext)
 										task.id = UUID()
@@ -117,84 +123,84 @@ struct FurtheranceApp: App {
 										task.stopTime = localDateTimeFormatter.date(from: columns[3])
 										furTasks.append(task)
 									}
-                                }
-                                try? persistenceController.container.viewContext.save()
-                            } else {
-                                showInvalidCSVAlert.toggle()
-                            }
-                        }
-                        fileURL.stopAccessingSecurityScopedResource()
-                    } catch {
-                        print("Failed to import data: \(error.localizedDescription)")
-                    }
-                }
-                .alert("Invalid CSV", isPresented: $showInvalidCSVAlert) {
-                    Button("OK") {}
-                } message: {
-                    Text("The CSV you chose is not a valid Furtherance CSV.")
-                }
-        }
-        .modelContainer(for: [
-            Shortcut.self,
-        ])
-        .commands {
-            CommandMenu("Database") {
-                Button("Export as CSV") {
-                    if storeModel.purchasedIds.isEmpty {
-                        showProAlert.toggle()
-                    } else {
-                        showExportCSV.toggle()
-                    }
-                }
-                .badge(storeModel.purchasedIds.isEmpty ? "Pro" : nil)
-                .disabled(tasksCount == 0)
-                Button("Import CSV") {
-                    if storeModel.purchasedIds.isEmpty {
-                        showProAlert.toggle()
-                    } else {
-                        showImportCSV.toggle()
-                    }
-                }
-                .badge(storeModel.purchasedIds.isEmpty ? "Pro" : nil)
-                Button("Delete All") {
-                    if showDeleteConfirmation {
-                        showDeleteDialog = true
-                        dialogTitle = "Delete all data?"
-                        dialogMessage = "This will delete all of your saved tasks."
-                        confirmBtn = "Delete"
-                    } else {
-                        deleteAllTasks()
-                    }
-                }
-                .disabled(tasksCount == 0)
-            }
-            #if os(macOS)
-            CommandGroup(replacing: CommandGroupPlacement.newItem) {}
-            CommandGroup(replacing: CommandGroupPlacement.windowList) {}
-            CommandGroup(replacing: CommandGroupPlacement.windowArrangement) {}
-            CommandGroup(replacing: CommandGroupPlacement.singleWindowList) {}
-            CommandGroup(before: CommandGroupPlacement.newItem) {
-                Button("Add Task") {
-                    addTaskSheet.toggle()
-                }
-                .keyboardShortcut("N", modifiers: EventModifiers.command)
-            }
-            #endif
-        }
+								}
+								try? persistenceController.container.viewContext.save()
+							} else {
+								showInvalidCSVAlert.toggle()
+							}
+						}
+						fileURL.stopAccessingSecurityScopedResource()
+					} catch {
+						print("Failed to import data: \(error.localizedDescription)")
+					}
+				}
+				.alert("Invalid CSV", isPresented: $showInvalidCSVAlert) {
+					Button("OK") {}
+				} message: {
+					Text("The CSV you chose is not a valid Furtherance CSV.")
+				}
+		}
+		.modelContainer(for: [
+			Shortcut.self,
+		])
+		.commands {
+			CommandMenu("Database") {
+				Button("Export as CSV") {
+					if storeModel.purchasedIds.isEmpty {
+						showProAlert.toggle()
+					} else {
+						showExportCSV.toggle()
+					}
+				}
+				.badge(storeModel.purchasedIds.isEmpty ? "Pro" : nil)
+				.disabled(tasksCount == 0)
+				Button("Import CSV") {
+					if storeModel.purchasedIds.isEmpty {
+						showProAlert.toggle()
+					} else {
+						showImportCSV.toggle()
+					}
+				}
+				.badge(storeModel.purchasedIds.isEmpty ? "Pro" : nil)
+				Button("Delete All") {
+					if showDeleteConfirmation {
+						showDeleteDialog = true
+						dialogTitle = "Delete all data?"
+						dialogMessage = "This will delete all of your saved tasks."
+						confirmBtn = "Delete"
+					} else {
+						deleteAllTasks()
+					}
+				}
+				.disabled(tasksCount == 0)
+			}
+			#if os(macOS)
+			CommandGroup(replacing: CommandGroupPlacement.newItem) {}
+			CommandGroup(replacing: CommandGroupPlacement.windowList) {}
+			CommandGroup(replacing: CommandGroupPlacement.windowArrangement) {}
+			CommandGroup(replacing: CommandGroupPlacement.singleWindowList) {}
+			CommandGroup(before: CommandGroupPlacement.newItem) {
+				Button("Add Task") {
+					addTaskSheet.toggle()
+				}
+				.keyboardShortcut("N", modifiers: EventModifiers.command)
+			}
+			#endif
+		}
 
-        #if os(macOS)
-        Settings {
-            SettingsView()
-        }
-        .defaultSize(width: 400, height: 450)
-        #endif
-    }
+		#if os(macOS)
+		Settings {
+			SettingsView()
+		}
+		.defaultSize(width: 400, height: 450)
+		#endif
+	}
 
-    private var mainContentView: some View {
-        #if os(macOS)
-        MacContentView(tasksCount: $tasksCount, showExportCSV: $showExportCSV)
-        #else
-        TimerView(tasksCount: $tasksCount, showExportCSV: $showExportCSV)
-        #endif
-    }
+	private var mainContentView: some View {
+		#if os(macOS)
+		MacContentView(tasksCount: $tasksCount, showExportCSV: $showExportCSV, showInspector: $showInspector, inspectorView: $inspectorView)
+		#else
+		TimerView(tasksCount: $tasksCount, showExportCSV: $showExportCSV)
+		#endif
+	}
 }
--- Furtherance/Views/macOS/MacContentView.swift
+++ Furtherance/Views/macOS/MacContentView.swift
@@ -8,98 +8,97 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
-
 struct MacContentView: View {
-    @Binding var tasksCount: Int
-    @Binding var showExportCSV: Bool
-    
-    @Environment(\.managedObjectContext) private var viewContext
+	@Binding var tasksCount: Int
+	@Binding var showExportCSV: Bool
+	@Binding var showInspector: Bool
+	@Binding var inspectorView: SelectedInspectorView
 
-    @ObservedObject var storeModel = StoreModel.shared
+	@Environment(\.managedObjectContext) private var viewContext
 
-    @StateObject var clickedGroup = ClickedGroup(taskGroup: nil)
-    @StateObject var clickedTask = ClickedTask(task: nil)
-    @StateObject var clickedShortcut = ClickedShortcut(shortcut: nil)
-    @StateObject var autosave = Autosave()
+	@ObservedObject var storeModel = StoreModel.shared
 
-    @AppStorage("defaultView") private var defaultView: NavItems = .timer
+	@StateObject var clickedGroup = ClickedGroup(taskGroup: nil)
+	@StateObject var clickedTask = ClickedTask(task: nil)
+	@StateObject var clickedShortcut = ClickedShortcut(shortcut: nil)
+	@StateObject var autosave = Autosave()
 
-    @State(initialValue: false) var showInspector: Bool
-    @State(initialValue: .editTask) var inspectorView: SelectedInspectorView
-    @State private var navSelection: NavItems? = .timer
+	@AppStorage("defaultView") private var defaultView: NavItems = .timer
 
-    // TODO: Create one observable object for everything here that needs to be changed by multiple views
-    var body: some View {
-        NavigationSplitView {
-            List(NavItems.allCases, id: \.self, selection: $navSelection) { navItem in
-                NavigationLink(navItem.rawValue.capitalized, value: navItem)
-            }
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-            Spacer()
-            if StopWatchHelper.shared.isRunning && navSelection != .timer {
-                TimeDisplayView()
-            }
-        } detail: {
-            if let selectedItem = navSelection {
-                switch selectedItem {
-                case .shortcuts: ShortcutsView(
-                        showInspector: $showInspector,
-                        inspectorView: $inspectorView,
-                        navSelection: $navSelection
-                    )
-                    .environmentObject(clickedShortcut)
-                case .timer: TimerView(tasksCount: $tasksCount, showExportCSV: $showExportCSV)
-                case .history: MacHistoryList(
-                        showInspector: $showInspector,
-                        inspectorView: $inspectorView,
-                        navSelection: $navSelection
-                    )
-                    .environmentObject(clickedGroup)
-                    .environmentObject(clickedTask)
+	@State private var navSelection: NavItems? = .timer
+
+	// TODO: Create one observable object for everything here that needs to be changed by multiple views
+	var body: some View {
+		NavigationSplitView {
+			List(NavItems.allCases, id: \.self, selection: $navSelection) { navItem in
+				NavigationLink(navItem.rawValue.capitalized, value: navItem)
+			}
+			.navigationSplitViewColumnWidth(min: 180, ideal: 200)
+			Spacer()
+			if StopWatchHelper.shared.isRunning && navSelection != .timer {
+				TimeDisplayView()
+			}
+		} detail: {
+			if let selectedItem = navSelection {
+				switch selectedItem {
+				case .shortcuts: ShortcutsView(
+						showInspector: $showInspector,
+						inspectorView: $inspectorView,
+						navSelection: $navSelection
+					)
+					.environmentObject(clickedShortcut)
+				case .timer: TimerView(tasksCount: $tasksCount, showExportCSV: $showExportCSV)
+				case .history: MacHistoryList(
+						showInspector: $showInspector,
+						inspectorView: $inspectorView,
+						navSelection: $navSelection
+					)
+					.environmentObject(clickedGroup)
+					.environmentObject(clickedTask)
 				case .report: ReportView()
-                }
-            } else {
-                TimerView(tasksCount: $tasksCount, showExportCSV: $showExportCSV)
-            }
-        }
-        .inspector(isPresented: $showInspector) {
-            switch inspectorView {
-            case .empty:
-                ContentUnavailableView("Nothing selected", systemImage: "cursorarrow.rays")
-                    .toolbar {
-                        if showInspector {
-                            ToolbarItem {
-                                Spacer()
-                            }
-                            ToolbarItem {
-                                Button {
-                                    showInspector = false
-                                } label: {
-                                    Image(systemName: "sidebar.trailing")
-                                        .help("Hide inspector")
-                                }
-                            }
-                        }
-                    }
-            case .editTaskGroup:
-                GroupView(showInspector: $showInspector)
-                    .environmentObject(clickedGroup)
-            case .editTask:
-                TaskEditView(showInspector: $showInspector)
-                    .environmentObject(clickedTask)
-                    .padding()
-            case .addShortcut:
-                AddShortcutView(showInspector: $showInspector)
-            case .editShortcut:
-                EditShortcutView(showInspector: $showInspector)
-                    .environmentObject(clickedShortcut)
-            }
-        }
-        .alert("Autosave Restored", isPresented: $autosave.showAlert) {
-            Button("OK") { Task { await autosave.read(viewContext: viewContext) } }
-        } message: {
-            Text("Furtherance shut down improperly. An autosave was restored.")
-        }
+				}
+			} else {
+				TimerView(tasksCount: $tasksCount, showExportCSV: $showExportCSV)
+			}
+		}
+		.inspector(isPresented: $showInspector) {
+			switch inspectorView {
+			case .empty:
+				ContentUnavailableView("Nothing selected", systemImage: "cursorarrow.rays")
+					.toolbar {
+						if showInspector {
+							ToolbarItem {
+								Spacer()
+							}
+							ToolbarItem {
+								Button {
+									showInspector = false
+								} label: {
+									Image(systemName: "sidebar.trailing")
+										.help("Hide inspector")
+								}
+							}
+						}
+					}
+			case .editTaskGroup:
+				GroupView(showInspector: $showInspector)
+					.environmentObject(clickedGroup)
+			case .editTask:
+				TaskEditView(showInspector: $showInspector)
+					.environmentObject(clickedTask)
+					.padding()
+			case .addShortcut:
+				AddShortcutView(showInspector: $showInspector)
+			case .editShortcut:
+				EditShortcutView(showInspector: $showInspector)
+					.environmentObject(clickedShortcut)
+			}
+		}
+		.alert("Autosave Restored", isPresented: $autosave.showAlert) {
+			Button("OK") { Task { await autosave.read(viewContext: viewContext) } }
+		} message: {
+			Text("Furtherance shut down improperly. An autosave was restored.")
+		}
 		// CSV Export
 		.fileExporter(
 			isPresented: $showExportCSV,
@@ -110,20 +109,20 @@ struct MacContentView: View {
 #if os(macOS)
 			.frame(minWidth: 360, idealWidth: 400, minHeight: 170, idealHeight: 600)
 #endif
-        .onAppear {
-            navSelection = defaultView
-            
-            Task {
-                await checkForAutosave()
-            }
-        }
-    }
-    
-    private func checkForAutosave() async {
-        if await autosave.exists() {
-            autosave.asAlert()
-        }
-    }
+			.onAppear {
+				navSelection = defaultView
+
+				Task {
+					await checkForAutosave()
+				}
+			}
+	}
+
+	private func checkForAutosave() async {
+		if await autosave.exists() {
+			autosave.asAlert()
+		}
+	}
 
 	private func dataAsCSV() -> String {
 		let allData: [FurTask] = fetchAllData()
@@ -156,5 +155,5 @@ struct MacContentView: View {
 }
 
 #Preview {
-    MacContentView(tasksCount: .constant(5), showExportCSV: .constant(false))
+	MacContentView(tasksCount: .constant(5), showExportCSV: .constant(false), showInspector: .constant(false), inspectorView: .constant(.empty))
 }
